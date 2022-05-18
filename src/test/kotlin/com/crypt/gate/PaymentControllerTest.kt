f7daaf049584fefe3c3769c5fcb87eca8f2e5db9
arksu <ark@ark.su>
2022-05-19 01:04:20 +0500

    tests

--- src/test/kotlin/com/crypt/gate/PaymentControllerTest.kt
+++ src/test/kotlin/com/crypt/gate/PaymentControllerTest.kt
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -17,8 +19,50 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 class PaymentControllerTest(
     @Autowired
     val mockMvc: MockMvc
-
 ) {
+    @Test
+    fun testValidateError() {
+        val body = "{\n" +
+                "  \"currency\": \"ETH\",\n" +
+                "  \"amount\": \"0.01\",\n" +
+                "  \"merchantId\": 1\n" +
+                "}"
+        mockMvc.perform(
+            post("/api/payment")
+                .content(body)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.timestamp", `is`(notNullValue())))
+            .andExpect(jsonPath("$.errors").isArray)
+            .andExpect(jsonPath("$.errors", hasSize<String>(2)))
+            .andExpect(jsonPath("$.errors", hasItem(containsString("paymentDTO.callbackUrl : Must not be null"))))
+            .andExpect(jsonPath("$.errors", hasItem(containsString("paymentDTO.callbackUrl : Can't be blank"))))
+            .andDo(print())
+    }
+
+    @Test
+    fun testValidateErrorBlank() {
+        val body = "{\n" +
+                "  \"currency\": \"ETH\",\n" +
+                "  \"amount\": \"0.01\",\n" +
+                "  \"merchantId\": 1,\n" +
+                "  \"callbackUrl\": \"\"\n" +
+                "}"
+        mockMvc.perform(
+            post("/api/payment")
+                .content(body)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.timestamp", `is`(notNullValue())))
+            .andExpect(jsonPath("$.errors").isArray)
+            .andExpect(jsonPath("$.errors", hasSize<String>(1)))
+            .andExpect(jsonPath("$.errors", hasItem(containsString("paymentDTO.callbackUrl : Can't be blank"))))
+            .andDo(print())
+    }
     @Test
     fun test404() {
         mockMvc.perform(get("/api/payment/9999").accept(MediaType.APPLICATION_JSON))
@@ -28,7 +72,6 @@ class PaymentControllerTest(
             .andExpect(jsonPath("$.errors", hasSize<String>(1)))
             .andExpect(jsonPath("$.errors", hasItem("Not found")))
             .andDo(print())
-            .andReturn()
     }
 
     @Test
@@ -40,6 +83,5 @@ class PaymentControllerTest(
             .andExpect(jsonPath("$.errors", hasSize<String>(1)))
             .andExpect(jsonPath("$.errors", hasItem("Wrong number format")))
             .andDo(print())
-            .andReturn()
     }
 }
