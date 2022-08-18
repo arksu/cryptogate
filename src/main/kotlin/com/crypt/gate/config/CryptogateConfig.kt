042c7dec007102018caf94ea25a5562931c413ab
arksu <ark@ark.su>
2022-08-18 11:46:03 +0500

    check eth address

--- src/main/kotlin/com/crypt/gate/config/CryptogateConfig.kt
+++ src/main/kotlin/com/crypt/gate/config/CryptogateConfig.kt
@@ -2,12 +2,26 @@ package com.crypt.gate.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import javax.annotation.PostConstruct
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "cryptogate")
 data class CryptogateConfig(
     val eth: EthConfig
-)
+) {
+    /**
+     * Проверим адреса, которые задали в конфиге
+     */
+    @PostConstruct
+    fun checkAddress () {
+        val regex = "^(0x)?[0-9a-f]{40}$".toRegex(setOf(RegexOption.IGNORE_CASE))
+        eth.wallets.forEach {
+            if (!regex.matches(it)) {
+                throw IllegalArgumentException("Wrong config ETH address $it")
+            }
+        }
+    }
+}
 
 data class EthConfig(
     val wallets: List<String>
